3d273e51555bf6e029b1b2c2c01bfee830307fa7
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-12-20 17:11:26 +0100

    add : aggiunto immagine al progetto

--- vite-hello-world/src/App.vue
+++ vite-hello-world/src/App.vue
@@ -1,22 +1,17 @@
 <script >
 import NewTitle from './components/NewTitle.vue'
+import Jambotron from './components/Jumbotron.vue'
 
 export default{
   components:{
-    NewTitle
+    NewTitle,
+    Jambotron
   }
 }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <Jambotron />
   <NewTitle />
 </template>
 
---
+++ vite-hello-world/src/components/Jumbotron.vue
@@ -0,0 +1,19 @@
+<script >
+export default{
+    name:"Jambotron"
+}
+</script>
+
+<template>
+  <div class="img-jambotron">
+    <!-- inserimento jambotron  -->
+  </div>
+</template>
+
+<style scoped>
+.img-jambotron{
+    background-image: url(../assets/th.jpg);
+    height: 100px;
+    width: 100%;
+}
+</style>
